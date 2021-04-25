3109ef6c066ad643d9270e2da9d547d13922a9a3
Andrew Scott <70250952+andrewbuildsthings@users.noreply.github.com>
2021-04-25 17:14:32 -0500

    Update calculator.py
    moved buttons around for better ui

--- calculator.py
+++ calculator.py
@@ -67,53 +67,53 @@ def button_equal():
 
 
 #buttons
-button_0 = Button(root, text="0", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 0))
-button_0.grid(row=4, column=0)
+button_0 = Button(root, text="0", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 0))
+button_0.grid(row=5, column=0)
 
-button_1 = Button(root, text="1", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 1))
-button_1.grid(row=3, column=0)
+button_1 = Button(root, text="1", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 1))
+button_1.grid(row=4, column=0)
 
-button_2 = Button(root, text="2", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 2))
-button_2.grid(row=3, column=1)
+button_2 = Button(root, text="2", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 2))
+button_2.grid(row=4, column=1)
 
-button_3 = Button(root, text="3", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 3))
-button_3.grid(row=3, column=2)
+button_3 = Button(root, text="3", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 3))
+button_3.grid(row=4, column=2)
 
-button_4 = Button(root, text="4", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 4))
-button_4.grid(row=2, column=0)
+button_4 = Button(root, text="4", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 4))
+button_4.grid(row=3, column=0)
 
-button_5 = Button(root, text="5", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 5))
-button_5.grid(row=2, column=1)
+button_5 = Button(root, text="5", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 5))
+button_5.grid(row=3, column=1)
 
-button_6 = Button(root, text="6", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 6))
-button_6.grid(row=2, column=2)
+button_6 = Button(root, text="6", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 6))
+button_6.grid(row=3, column=2)
 
-button_7 = Button(root, text="7", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 7))
-button_7.grid(row=1, column=0)
+button_7 = Button(root, text="7", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 7))
+button_7.grid(row=2, column=0)
 
-button_8 = Button(root, text="8", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 8))
-button_8.grid(row=1, column=1)
+button_8 = Button(root, text="8", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 8))
+button_8.grid(row=2, column=1)
 
-button_9 = Button(root, text="9", bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 9))
-button_9.grid(row=1, column=2)
+button_9 = Button(root, text="9", bd=4, bg="#cfcfd8", padx=40, pady=20, command=partial(button_click, 9))
+button_9.grid(row=2, column=2)
 
-button_add = Button(root, text="+", bg="#e0dab5", padx=40, pady=20, command=button_add)
-button_add.grid(row=4, column=3)
+button_add = Button(root, text="+", bd=4, bg="#e0dab5", padx=40, pady=20, command=button_add)
+button_add.grid(row=1, column=0)
 
-button_sub = Button(root, text="-", bg="#e0dab5", padx=40, pady=20, command=button_sub)
-button_sub.grid(row=3, column=3)
+button_sub = Button(root, text="-", bd=4, bg="#e0dab5", padx=40, pady=20, command=button_sub)
+button_sub.grid(row=1, column=1)
 
-button_mul = Button(root, text="*", bg="#e0dab5", padx=40, pady=20, command=button_mul)
-button_mul.grid(row=2, column=3)
+button_mul = Button(root, text="*", bd=4, bg="#e0dab5", padx=40, pady=20, command=button_mul)
+button_mul.grid(row=1, column=2)
 
-button_div = Button(root, text="/", bg="#e0dab5", padx=40, pady=20, command=button_div)
+button_div = Button(root, text="/", bd=4, bg="#e0dab5", padx=40, pady=20, command=button_div)
 button_div.grid(row=1, column=3)
 
-button_tot = Button(root, text="=", bg="#e0dab5", padx=40, pady=20, command=button_equal)
-button_tot.grid(row=4, column=2)
+button_tot = Button(root, text="=", bd=4, bg="#e0dab5", padx=89, pady=20, command=button_equal)
+button_tot.grid(row=5, columnspan=2, column=1)
 
-button_clr = Button(root, text="C", bg="#a3c9ec", padx=40, pady=20, command=button_clear)
-button_clr.grid(row=4, column=1)
+button_clr = Button(root, text="C", bd=4, bg="#a3c9ec", padx=40, pady=90, command=button_clear)
+button_clr.grid(row=3, rowspan=3, column=3)
 
 
 root.mainloop()
